9cc645da496ffecbc2742dd73966c735cf09eac9
Chihcheng Hsieh <mike820808@gmail.com>
2019-12-17 21:30:58 +1000

    testing websocket

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"Quenc/database"
-	"Quenc/routers"
-
 	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+	"quenc/database"
+	"quenc/routers"
 )
 
 func main() {
 	gin.ForceConsoleColor()
 	database.InitDB()
-
 	r := routers.InitRouter()
-
 	r.Run()
 }
--- routers/index.go
+++ routers/index.go
@@ -1,6 +1,17 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 // InitRouter -initialise all the routers
 func InitRouter() *gin.Engine {
@@ -8,6 +19,40 @@ func InitRouter() *gin.Engine {
 
 	router.GET("/test", func(c *gin.Context) {
 		c.JSON(200, []string{"123", "321"})
+	})
+
+	router.GET("/ws", func(c *gin.Context) {
+
+		// Upgrade writer and Reader
+		ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+
+		defer ws.Close()
+
+		if err != nil {
+			log.Printf("The websocket is not working due to the error : %+v \n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		for {
+			mt, message, err := ws.ReadMessage()
+			if err != nil {
+				log.Printf("Error occur: %+v\b", err)
+				break
+			}
+
+			if string(message) == "ping" {
+				message = []byte("pong")
+			}
+
+			err = ws.WriteMessage(mt, message)
+
+			if err != nil {
+				break
+			}
+		}
 	})
 
 	return router
