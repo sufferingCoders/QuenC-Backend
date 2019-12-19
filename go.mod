d415117f85b4ec66cde275af67cf048dcc5e0414
Chihcheng Hsieh <mike820808@gmail.com>
2019-12-20 02:31:40 +1000

    successful connect to mongodb and flutter front-end

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/gorilla/websocket v1.4.1
 	github.com/joho/godotenv v1.3.0
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/stretchr/testify v1.4.0
 	github.com/tidwall/pretty v1.0.0 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
--- routers/index.go
+++ routers/index.go
@@ -1,16 +1,34 @@
 package routers
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
+	"context"
 	"log"
 	"net/http"
+	"quenc/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var upGrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+// Testing - for testing
+type Testing struct {
+	ID    primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	Email string             `json:"email" bson:"email"`
+}
+
+// TestInfo - for adddin the test
+type TestInfo struct {
+	Email string `json:"email" bson:"email"`
 }
 
 // InitRouter -initialise all the routers
@@ -20,6 +38,159 @@ func InitRouter() *gin.Engine {
 	router.GET("/test", func(c *gin.Context) {
 		c.JSON(200, []string{"123", "321"})
 	})
+
+	// Inserting the test value
+	router.POST("/test", func(c *gin.Context) {
+		var testAdding TestInfo
+		err := c.ShouldBindJSON(&testAdding)
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"err": err,
+				"msg": "Cannot bind the given info",
+			})
+			return
+		}
+
+		testingClient := Testing{
+			Email: testAdding.Email,
+		}
+
+		result, err := database.DB.Collection("test").InsertOne(context.TODO(), testingClient)
+
+		if err != nil {
+			log.Printf("cannot insert a test due to the error : %+v \n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"id": result.InsertedID,
+		})
+
+	})
+
+	// Changing the value of a test
+	router.PUT("/test/:id", func(c *gin.Context) {
+
+		id := c.Param("id")
+
+		oid, err := primitive.ObjectIDFromHex(id)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err,
+				"msg": "The given id cannot be transform to oid",
+			})
+			return
+		}
+
+		var testInfo TestInfo
+		err = c.ShouldBindJSON(&testInfo)
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"err": err,
+				"msg": "Cannot bind the given info",
+			})
+			return
+		}
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"err": err,
+				"msg": "Cannot bind the given info with Products",
+			})
+			return
+		}
+
+		result, err := database.DB.Collection("test").UpdateOne(
+			context.TODO(),
+			bson.M{"_id": oid},
+			bson.M{"$set": bson.M{"email": testInfo.Email}},
+		)
+
+		if err != nil {
+			log.Printf("cannot update a test due to the error : %+v \n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"UpsertedID": result.UpsertedID,
+		})
+
+	})
+
+	// This for creating the WebSocket and listen to the change stream in the MongoDB
+	router.GET("/test/subscribe/:id", func(c *gin.Context) {
+
+		// add the user first
+		println("IN sub")
+		ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+
+		defer ws.Close()
+
+		if err != nil {
+			log.Printf("The websocket is not working due to the error : %+v \n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		id := c.Param("id")
+
+		oid, err := primitive.ObjectIDFromHex(id)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err,
+				"msg": "The given id cannot be transform to oid",
+			})
+			return
+		}
+
+		pipeline := mongo.Pipeline{bson.D{{"$match", bson.D{{"fullDocument._id", oid}}}}}
+
+		collectionStream, err := database.DB.Collection("test").Watch(context.TODO(), pipeline, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+
+		if err != nil {
+			log.Printf("The collection stream is not working due to the error : %+v \n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err,
+			})
+			return
+		}
+
+		defer collectionStream.Close(context.TODO())
+
+		for {
+			ok := collectionStream.Next(context.TODO())
+			if ok {
+				next := collectionStream.Current
+
+				log.Printf("Next: %+v", next)
+
+				err = ws.WriteMessage(websocket.TextMessage, []byte(next.String()))
+
+				if err != nil {
+					break
+				}
+			}
+
+			// log.Printf("The collection stream cannot perform next")
+			// c.JSON(http.StatusInternalServerError, gin.H{
+			// 	"error": "The collection stream cannot perform next",
+			// })
+		}
+	})
+
+	// This for changing the filed in the MongoDB
 
 	router.GET("/ws", func(c *gin.Context) {
 
